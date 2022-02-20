a5f4a4a6afeb5256ddbd266b8c2acc384e07b468
limzixiang007 <xiang0072000live.com@gmail.com>
2022-02-20 16:19:19 +0800

    PlayerMovement
    PlayerMovement

---
+++ Project 4TOM/Assets/Player/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed;
+    public float jumpSpeed;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D myFeet;
+    private bool isGround;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Facedirection();
+        Jump();
+        CheckGround();
+        SwitchAnimation();
+    }
+
+    void CheckGround()
+    {
+        isGround = myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")) ||
+            myFeet.IsTouchingLayers(LayerMask.GetMask("MovingPlatform"));
+    }
+
+
+    void Run()
+    {
+        float move01 = Input.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(move01 * runSpeed, rb.velocity.y);
+        rb.velocity = playerVel;
+        bool PlayerRunSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        anim.SetBool("Run", PlayerRunSpeed);
+    }
+
+
+    void Facedirection()
+    {
+        bool PlayerRunSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (PlayerRunSpeed)
+        {
+            if (rb.velocity.x < 0.0f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            if (rb.velocity.x > 0.0f)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+        }
+    }
+
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGround)
+            {
+
+                Vector2 jumpVel = new Vector2(0.0f, jumpSpeed);
+                rb.velocity = Vector2.up * jumpVel;
+                anim.SetBool("Jump", true);
+            }
+        }
+    }
+
+    void SwitchAnimation()
+    {
+        anim.SetBool("Idle", false);
+        if (anim.GetBool("Jump"))
+        {
+            if (rb.velocity.y < 0.0f)
+            {
+                anim.SetBool("Jump", false);
+                anim.SetBool("Fall", true);
+            }
+        }
+        else if (isGround)
+        {
+            anim.SetBool("Fall", false);
+            anim.SetBool("Idle", true);
+        }
+    }
+}
