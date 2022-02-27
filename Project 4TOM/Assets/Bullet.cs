373e2bc29b7011b0bb9dda86c43c349bd3ba1d8d
KaleidoLee <panli.hong82@gmail.com>
2022-02-27 13:11:07 +0800

    Bullet code update
    * bullet now will die out of time instead of number of bounces
    * line renderer updated but does not work in this project
    * changed the location of fire point to the top of the player

--- Project 4TOM/Assets/Bullet.cs
+++ Project 4TOM/Assets/Bullet.cs
@@ -5,40 +5,53 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] LayerMask collisionMask;
-    [SerializeField] float speed = 20f;
+    public float speed = 20f;
     [SerializeField] Rigidbody2D rb;
-    [SerializeField] int maxBounceNum = 3;
+    //[SerializeField] int maxBounceNum = 3;
     [SerializeField] GameObject playerChar;
+    [SerializeField] float bulletLifetimeLimit = 5f;
 
     Vector3 lastVelocity;
+    float bulletLifetime = 0f;
 
-    int bounceNum = 0;
+    //int bounceNum = 0; 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = transform.right * -speed;
+        rb.velocity = transform.right * -speed; //speed is given to bullet when initiating bullet in Skill_1_Ricochet script
     }
 
     private void Update()
     {
-        lastVelocity = rb.velocity;
-        Physics2D.IgnoreLayerCollision(3, 7);
+        lastVelocity = rb.velocity; //keep updating direction even after bouncing with another platform
+        Physics2D.IgnoreLayerCollision(3, 7); //bullet ignore player layer
+
+        if (bulletLifetime <= bulletLifetimeLimit)
+        {
+            bulletLifetime += Time.deltaTime;
+        }
+        else //if bullet spends more than bulletLifetimeLimit on screen
+        {
+            Destroy(this.gameObject);
+        }
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
         {
             var speed = lastVelocity.magnitude;
-            var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+            var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal); //reflect to bounce against platform
 
-            rb.velocity = direction * Mathf.Max(speed, 0f);
-            bounceNum++;
-            
-            if (bounceNum >= maxBounceNum)
-            {
-                Destroy(gameObject);
-            }
+            rb.velocity = direction * Mathf.Max(speed, 0f); //change rigidbody direction if contact with another platform
+
+            //bounceNum++;
+
+            //if (bounceNum >= maxBounceNum)
+            //{
+            //    Destroy(gameObject);
+            //}
         }
     }
 }
--- Project 4TOM/Assets/PlayerScript/Skill_1_Ricochet.cs
+++ Project 4TOM/Assets/PlayerScript/Skill_1_Ricochet.cs
@@ -4,29 +4,72 @@ using UnityEngine;
 
 public class Skill_1_Ricochet : MonoBehaviour
 {
-    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject firePoint;
     [SerializeField] Camera cam;
     [SerializeField] GameObject bulletPrefab;
 
     public Vector2 lookDirection;
     public float lookAngle;
 
+    LineRenderer lr;
+
+    private void Start()
+    {
+        lr = firePoint.GetComponent<LineRenderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        lookDirection = cam.ScreenToWorldPoint(Input.mousePosition); //get Vector2 component of mouse position on screen
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 180f; //calculate angle between firepoint and mouse position
+        firePoint.transform.rotation = Quaternion.Euler(0, 0, lookAngle); //firepoint rotates towards mouse position
+
+        //create a line of trajectory 
+        Vector2[] trajectory = Plot(bulletPrefab.GetComponent<Rigidbody2D>(), firePoint.transform.position, lookDirection, 500);
+
+        //create a new point in the line renderer for each element in the list
+        lr.positionCount = trajectory.Length;
+
+        Vector3[] positions = new Vector3[trajectory.Length];
+        for (int i = 0; i < trajectory.Length; i++)
+        {
+            positions[i] = trajectory[i];
+        }
+        lr.SetPositions(positions); //set position of points for each element in Vector3[] positions
+
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
-
-        lookDirection = cam.ScreenToWorldPoint(Input.mousePosition);
-        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 250f;
-        firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
     }
 
     void Shoot()
     {
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Instantiate(bulletPrefab, firePoint.transform.position, firePoint.transform.rotation);
+
+    }
+
+    //function to plot each points in the preview line for line renderer
+    public Vector2[] Plot(Rigidbody2D rigidbody, Vector2 pos, Vector2 velocity, int steps)
+    {
+        Vector2[] results = new Vector2[steps]; //create a new Vector2 list
+
+        float timestep = Time.fixedDeltaTime / Physics2D.velocityIterations;
+        Vector2 gravityAccel = Physics2D.gravity * rigidbody.gravityScale * timestep * timestep;
+
+        float drag = 1f - timestep * rigidbody.drag;
+        Vector2 moveStep = velocity * timestep;
+
+        for (int i = 0; i < steps; i++)
+        {
+            moveStep += gravityAccel;
+            moveStep *= drag;
+            pos += moveStep;
+            results[i] = pos; //put the Vector2 position into list results[]
+        }
+
+        return results; //return list
     }
 }
 
